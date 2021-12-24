02791bec40a089b80b50d71c3210a1a07e25c127
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-12-24 04:10:51 -0500

    Update setDefaultAccount.js

--- Scripts/setDefaultAccount.js
+++ Scripts/setDefaultAccount.js
@@ -1,18 +1,18 @@
 module.exports = function(web3) {
   if (process.argv[4] === '--staging') {
-    const account = web3.eth.accounts.pop()
-    console.log(`Setting default account to the last account at web3.eth.accounts and unlocking it: ${account}`)
+    const account = web3.avax.accounts.pop()
+    console.log(`Setting default account to the last account at web3.avax.accounts and unlocking it: ${account}`)
     web3.personal.unlockAccount(account, '123456789', 24 * 3600)
     console.log(`${account} unlocked`)
-    web3.eth.defaultAccount = account
+    web3.avax.defaultAccount = account
   } else if (process.argv[4] === '--release') {
     const account = web3.eth.accounts.pop()
-    console.log(`Setting default account to the last account at web3.eth.accounts and unlocking it: ${account}`)
+    console.log(`Setting default account to the last account at web3.avax.accounts and unlocking it: ${account}`)
     web3.personal.unlockAccount(account, '123456789', 24 * 3600)
     console.log(`${account} unlocked`)
-    web3.eth.defaultAccount = account
+    web3.avax.defaultAccount = account
   } else {
-    console.log(`Setting default account to the last account at web3.eth.accounts: ${web3.eth.accounts.pop()}`)
-    web3.eth.defaultAccount = web3.eth.accounts.pop()
+    console.log(`Setting default account to the last account at web3.avax.accounts: ${web3.avax.accounts.pop()}`)
+    web3.avax.defaultAccount = web3.avax.accounts.pop()
   }
 }
